64ed9a748c0c694b8b9ee4cbcb950184581a19b6
10kookies <neerajabeesetti10@gmail.com>
2019-03-04 22:31:03 -0800

    debugged parseResults and made new method for removing commas inside quotations (not generalized)

--- src/Utils.java
+++ src/Utils.java
@@ -21,51 +21,52 @@ public class Utils {
         return output.toString();
     }
 
-    public static ArrayList<ElectionResult>  parse2016ElectionResults(String data) {
+    public static ArrayList<ElectionResult> parse2016ElectionResults(String data) {
         ArrayList<ElectionResult> results = new ArrayList<>();
 
         String[] rows = data.split("\n");
 
         for (int i = 1; i < rows.length; i++) {
-            String stringDiff;
-            int diff = 0;
-            int stringStartIndex = rows[i].indexOf("\"");
-
-            if (stringStartIndex != -1) {
-                int stringEndIndex = rows[i].indexOf("\"", stringStartIndex + 1);
-
-                stringDiff = rows[i].substring(stringStartIndex+1, stringEndIndex );
-                stringDiff = stringDiff.replace(",", ""); //implement something so that stuff w/o "" also included
-                diff = Integer.parseInt(stringDiff);
-
-            }
+            removeCommasInQuotations(rows, i);
 
             String[] line = rows[i].split(",");
-
-            System.out.println(line[0]);
 
             double votesDem = Double.parseDouble(line[1]);
             double votesGop = Double.parseDouble(line[2]);
             double totalVotes = Double.parseDouble(line[3]);
             double perDem = Double.parseDouble(line[4]);
             double perGop = Double.parseDouble(line[5]);
-
-            if (stringStartIndex == -1) diff = Integer.parseInt(line[6]);
-
-            String perPointDiffWOPercentSign = line[7].substring(0, line[7].length()-1);
-            double perPointDiff = Double.parseDouble(perPointDiffWOPercentSign); //input is 410, should be 15.17
-
+            int diff = Integer.parseInt(line[6]);
+            double perPointDiff = Double.parseDouble(line[7].substring(0, line[7].length() - 1));
             String stateAbbr = line[8];
             String countryName = line[9];
-            int combinedFips = Integer.parseInt(line[10]); //input is Alaska
-            //TODO: fix the indexes
+            int combinedFips = Integer.parseInt(line[10]);
 
-           ElectionResult result = new ElectionResult(votesDem, votesGop, totalVotes, perDem, perGop, diff, perPointDiff, stateAbbr, countryName, combinedFips);
-           results.add(result);
+            ElectionResult result = new ElectionResult(votesDem, votesGop, totalVotes, perDem, perGop, diff, perPointDiff, stateAbbr, countryName, combinedFips);
+            result.resultToString();
+            results.add(result);
 
         }
-
         return results;
+    }
 
+    private static void removeCommasInQuotations(String[] rows, int i) {
+        String stringDiff;
+        String diffWComma;
+        String diffWOComma;
+        int stringStartIndex = rows[i].indexOf("\"");
+
+        if (stringStartIndex != -1) {
+            int stringEndIndex = rows[i].indexOf("\"", stringStartIndex + 1);
+
+            rows[i] = rows[i].replace("\"", "");
+            stringDiff = rows[i].substring(stringStartIndex, stringEndIndex - 1);
+            diffWComma = stringDiff;
+            stringDiff = stringDiff.replace(",", "");
+            diffWOComma = stringDiff;
+
+            rows[i] = rows[i].replace(diffWComma, diffWOComma);
+
+        }
     }
 }
