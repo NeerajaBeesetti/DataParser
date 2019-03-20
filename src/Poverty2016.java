77965d29f1c537e6c7318dd51edcf44987205985
10kookies <neerajabeesetti10@gmail.com>
2019-03-20 11:10:09 -0700

    made Poverty2016 class with getters and setters

---
+++ src/Poverty2016.java
@@ -0,0 +1,18 @@
+public class Poverty2016 {
+
+    private double incomeBelowPov;
+
+    public Poverty2016() {
+
+    }
+
+    public double getIncomeBelowPov() {
+        return incomeBelowPov;
+    }
+
+    public void setIncomeBelowPov(double incomeBelowPov) {
+        this.incomeBelowPov = incomeBelowPov;
+    }
+
+
+}
